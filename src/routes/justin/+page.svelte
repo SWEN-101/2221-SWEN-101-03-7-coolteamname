95e8cf48bfd5418c067aa48a19d7b595f878def0
hiromon0125 <hiroto125takeuchi@gmail.com>
2022-11-03 19:40:09 -0400

    fix: scroll and format on the map

--- src/routes/justin/+page.svelte
+++ src/routes/justin/+page.svelte
@@ -1,29 +1,27 @@
 <script>
 	// @ts-nocheck
 
-    // Uses the map box library in Svelte
-    // https://github.com/beyonk-adventures/svelte-mapbox?ref=madewithsvelte.com
+	// Uses the map box library in Svelte
+	// https://github.com/beyonk-adventures/svelte-mapbox?ref=madewithsvelte.com
 	import { Map, Geocoder, Marker, controls } from '@beyonk/svelte-mapbox';
 	import { onMount } from 'svelte';
 
 	const { GeolocateControl, NavigationControl, ScaleControl } = controls;
 
-    let listCoor = [
-        {name: "RIT HealthCenter",coor: [-77.67217267797808,43.08419644348089]},
-    ]
+	let listCoor = [{ name: 'RIT HealthCenter', coor: [-77.67217267797808, 43.08419644348089] }];
 
-    let mapComponent;
-    let itemIndex = 0;
-    $: centeredHosp = listCoor[itemIndex];
-    $: lng = centeredHosp.coor[0];
-    $: lat = centeredHosp.coor[1];
-    // for some reason 1 is the lowest zoom you can do
-    let zoom = 14;
-    onMount(()=>{
-        // Usage of methods like setCenter and flyto
-        mapComponent.setCenter([lng, lat]); // zoom is optional
-        mapComponent.flyTo({ center: [lng, lat], zoom}); // documentation (https://docs.mapbox.com/mapbox-gl-js/example/flyto)
-    })
+	let mapComponent;
+	let itemIndex = 0;
+	$: centeredHosp = listCoor[itemIndex];
+	$: lng = centeredHosp.coor[0];
+	$: lat = centeredHosp.coor[1];
+	// for some reason 1 is the lowest zoom you can do
+	let zoom = 14;
+	onMount(() => {
+		// Usage of methods like setCenter and flyto
+		mapComponent.setCenter([lng, lat]); // zoom is optional
+		mapComponent.flyTo({ center: [lng, lat], zoom }); // documentation (https://docs.mapbox.com/mapbox-gl-js/example/flyto)
+	});
 
 	// Define this to handle `eventname` events - see [GeoLocate Events](https://docs.mapbox.com/mapbox-gl-js/api/markers/#geolocatecontrol-events)
 	function eventHandler(e) {
@@ -36,33 +34,42 @@
 		'pk.eyJ1IjoiaGVyb3BvbiIsImEiOiJjbGExbnAxcnkwOWZ3M3VwcWN5OW0xZmJjIn0.LhqWR4pWb1qsc240GqTuEA';
 </script>
 
+<div class="spacer" />
 <div class="main">
-    <Map
-        accessToken={api_key}
-        bind:this={mapComponent}
-        on:recentre={(e) => console.log(e.detail.center.lat, e.detail.center.lng)}
-        options={{ scrollZoom: true }}
-    >
-        <Marker
-            lat={lat}
-            lng={lng}
-            color="rgb(255,255,255)"
-            label="some marker label"
-            popupClassName="class-name"
-        />
-        <NavigationControl />
-        <GeolocateControl options={{ some: 'control-option' }} on:eventname={eventHandler} />
-        <ScaleControl />
-    </Map>
+	<Map
+		accessToken={api_key}
+		bind:this={mapComponent}
+		on:recentre={(e) => console.log(e.detail.center.lat, e.detail.center.lng)}
+		options={{ scrollZoom: true }}
+	>
+		<Marker
+			{lat}
+			{lng}
+			color="rgb(255,255,255)"
+			label="some marker label"
+			popupClassName="class-name"
+		/>
+		<NavigationControl />
+		<GeolocateControl options={{ some: 'control-option' }} on:eventname={eventHandler} />
+		<ScaleControl />
+	</Map>
 </div>
 
 <style>
-    .main {
-        height: 100vh;
-        width: 100vw;
+    .spacer {
+        height: 1rem;
     }
-    :global(.mapboxgl-map) {
-        height: 200px;
-        width: 200px;
-    }
+	.main {
+		position: relative;
+		height: calc(100vh - 4rem);
+		width: 100%;
+		z-index: -1;
+		padding: 0;
+	}
+	:global(.mapboxgl-map) {
+		height: 100px;
+		width: 100px;
+		color: red;
+		margin: 0;
+	}
 </style>
