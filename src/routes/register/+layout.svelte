6d9444ed0ca593452aac69494f6eb4092ffe0fd0
hiromon0125 <hiroto125takeuchi@gmail.com>
2022-11-05 19:42:41 -0400

    fix: header layout

---
+++ src/routes/register/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	// @ts-nocheck
+
+	import EmptyHeader from '../EmptyHeader.svelte';
+</script>
+
+<!-- @author Hiroto signup page -->
+
+<EmptyHeader defaultShrink={true}>
+	<div slot="title">
+		<h1>Sign Up</h1>
+	</div>
+
+	<div slot="link" />
+
+	<span slot="profile" />
+</EmptyHeader>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,20 +1,3 @@
-<script>
-	// @ts-nocheck
-
-	import EmptyHeader from '../EmptyHeader.svelte';
-</script>
-
-<!-- @author Hiroto signup page -->
-
-<EmptyHeader defaultShrink={true}>
-	<div slot="title">
-		<h1>Sign Up</h1>
-	</div>
-
-	<div slot="link" />
-
-	<span slot="profile" />
-</EmptyHeader>
 
 <div class="spacer" />
 
