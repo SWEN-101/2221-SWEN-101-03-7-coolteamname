cc34f388175852e8f80653f8414ab29e2afbe121
Katie Lawson <krl2943@g.rit.edu>
2022-11-16 20:45:11 -0500

    edited heading size and added line break -- working on formatting the text and box sizing

--- src/routes/(login-required)/katie/+page.svelte
+++ src/routes/(login-required)/katie/+page.svelte
@@ -1,9 +1,18 @@
 <body>
-    <h2>History</h2>
+    <h1>History</h1>
+    <hr />
 
     <div class="segments">
         <div>
-            <p>This is where appointments will be</p>
+            <p>
+                Let's see if this changes anything
+            </p>
+            <p>
+                Ok
+            </p>
+            <p>
+                It looks like doing separate paragraphs adds space between the lines of text. Good to know
+            </p>
         </div>
         <div>
             <p>This is where past exams/tests will be</p>
@@ -17,16 +26,15 @@
 <style>
     .segments {
         display: flex;
-        align-items: center;
-        height: 250px;
+        align-items: left;
+        height: 480px;
         background-color: white;
     }
 
     .segments > div {
-        text-align: right;
-        width: 90px;
-        margin: 10px;
-        flex: 1 1 200px;
+        text-align: left;
+        width: 500px;
+        margin: 30px;
         box-shadow: 4px 5px 11px 10px lightgray;
 
     }
