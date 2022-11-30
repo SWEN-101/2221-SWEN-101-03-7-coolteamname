404741b693b1eecff381815fdf92896918e8d8e4
NoMoreTp <ax7892@gmail.com>
2022-11-29 21:07:22 -0500

    appointment stuff

---
+++ src/routes/(login-required)/andy/make appointment/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Header from "$lib/components/Header.svelte";
+
+</script>
+<!-- Andy's html file -->
+
+
+<body>
+    <head>Stealth
+        <p>
+            Stealth
+        </p>
+    </head>
+
+    <body4>
+        <p>
+            --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        </p>
+    </body4>
+    <h1>
+        Make Appointment
+    </h1>
+
+    <body4>
+        <p>
+            --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        </p>
+    </body4>
+
+    <body2>
+        <form action="/action_page.php">
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname"><br><br>
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"><br><br>
+            <label for="hname">Hospital name:</label>
+            <input type="text" id="hname" name="hname"><br><br>
+            <label for="time">Appointment time:</label>
+            <input type="text" id="time" name="time"><br><br>
+            <button onclick="window.location='andy/appointment list/appointment made';">Submit</button>    
+        </form>    </body2>
+
+
+</body>
---
+++ src/routes/(login-required)/andy/make appointment/appointment made/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Header from "$lib/components/Header.svelte";
+
+</script>
+<!-- Andy's html file -->
+
+
+<body>
+	<head>Stealth
+        <p>
+            Stealth
+        </p>
+    </head>
+
+	<h2>
+		<p>
+			You have successfully made an appointment
+		</p>
+		
+		<button onclick="window.location='andy/appointment list';">Make another appointment</button>    
+
+	</h2>
+</body>
