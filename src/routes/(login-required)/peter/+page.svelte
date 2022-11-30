99ae9b0cffb667d5fea7c20719fe21b543b90381
JzJoker <justinzhao1324@gmail.com>
2022-11-30 18:13:46 -0500

    coming soon peter

--- src/routes/(login-required)/peter/+page.svelte
+++ src/routes/(login-required)/peter/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Medical Information</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec sagittis aliquam malesuada bibendum arcu. Dignissim sodales ut eu sem integer vitae. Duis at tellus at urna</p>
+<p>Coming Soon...</p>
 <!-- @author Hiroto Takeuchi -->
