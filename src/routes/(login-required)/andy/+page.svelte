5d42ddbc79888f8d136d39e2f83504b9dbf758d8
hiromon0125 <hiroto125takeuchi@gmail.com>
2022-11-30 18:01:34 -0500

    fix andy's files

--- src/routes/(login-required)/andy/+page.svelte
+++ src/routes/(login-required)/andy/+page.svelte
@@ -1,15 +1,21 @@
 <script>
-	import Header from "$lib/components/Header.svelte";
-
+	import { goto } from '$app/navigation';
+	import Header from '$lib/components/Header.svelte';
 </script>
+
 <!-- Andy's html file -->
 
-
 <body>
-
-    <h1>Appointments</h1>
-    <button onclick="window.location='andy/make appointment';">Make Appointments</button>    
-    <p></p>
-    <button onclick="window.location='andy/appointment list';">View Appointments</button>    
-
+	<h1>Appointments</h1>
+	<button
+		on:click={() => {
+			goto('create');
+		}}>Make Appointments</button
+	>
+	<p />
+	<button
+		on:click={() => {
+			goto('list');
+		}}>View Appointments</button
+	>
 </body>
---
+++ src/routes/(login-required)/andy/create/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { goto } from '$app/navigation';
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<!-- Andy's html file -->
+
+<body>
+	<hr />
+	<h1>Make Appointment</h1>
+	<hr />
+
+	<body2>
+		<form action="make appointment/appointment made">
+			<label for="fname">First name:</label>
+			<input type="text" id="fname" name="fname" /><br /><br />
+			<label for="lname">Last name:</label>
+			<input type="text" id="lname" name="lname" /><br /><br />
+			<label for="reason">Reason for visit:</label>
+			<input type="text" id="reason" name="reason" /><br /><br />
+
+			<label for="hname">Hospital name:</label>
+			<input type="location" id="hname" name="hname" /><br /><br />
+			<label for="dname">Doctor:</label>
+			<input type="text" id="dname" name="dname" /><br /><br />
+
+			<label for="date">Appointment date:</label>
+			<input type="date" id="date" name="date" /><br /><br />
+			<label for="time">Appointment time:</label>
+			<input type="time" id="time" name="time" /><br /><br />
+
+			<button onclick="window.location='make appointment/appointment made';">Submit</button>
+		</form>
+	</body2>
+    <hr/>
+    <button on:click={()=>{goto("./list")}}>view appointments</button>
+</body>
