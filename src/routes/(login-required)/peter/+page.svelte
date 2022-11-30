23de5058b7312072cb264ce8a688d661e29627e3
JzJoker <justinzhao1324@gmail.com>
2022-11-30 17:45:17 -0500

    peter code

---
+++ src/routes/(login-required)/peter/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Medical Information</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec sagittis aliquam malesuada bibendum arcu. Dignissim sodales ut eu sem integer vitae. Duis at tellus at urna</p>
+<!-- @author Hiroto Takeuchi -->
